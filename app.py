fb849297ba3b0259fae0ca2f76a854567eafa0a4
Dark0Spirit <2424592449@qq.com>
2020-12-31 21:09:35 +0800

    update README.md

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import jsonify
-from jieba.analyse import  extract_tags
+from jieba.analyse import extract_tags
 import string
 import utils
 
@@ -13,30 +13,34 @@ app = Flask(__name__)
 def hello_world():
     return render_template("main.html")
 
+
 @app.route("/c1")
 def get_c1_data():
     data = utils.get_c1_data()
-    return jsonify({"confirm":data[0],"suspect":data[1],"heal":data[2],"dead":data[3]})
+    return jsonify({"confirm": data[0], "suspect": data[1], "heal": data[2], "dead": data[3]})
+
 
 @app.route("/c2")
 def get_c2_data():
     res = []
     for tup in utils.get_c2_data():
         # print(tup)
-        res.append({"name":tup[0],"value":int(tup[1])})
-    return jsonify({"data":res})
+        res.append({"name": tup[0], "value": int(tup[1])})
+    return jsonify({"data": res})
+
 
 @app.route("/l1")
 def get_l1_data():
     data = utils.get_l1_data()
-    day,confirm,suspect,heal,dead = [],[],[],[],[]
-    for a,b,c,d,e in data[7:]:
-        day.append(a.strftime("%m-%d")) #a是datatime类型
+    day, confirm, suspect, heal, dead = [], [], [], [], []
+    for a, b, c, d, e in data[7:]:
+        day.append(a.strftime("%m-%d"))  # a是datatime类型
         confirm.append(b)
         suspect.append(c)
         heal.append(d)
         dead.append(e)
-    return jsonify({"day":day,"confirm": confirm, "suspect": suspect, "heal": heal, "dead": dead})
+    return jsonify({"day": day, "confirm": confirm, "suspect": suspect, "heal": heal, "dead": dead})
+
 
 @app.route("/l2")
 def get_l2_data():
@@ -48,12 +52,13 @@ def get_l2_data():
         suspect_add.append(c)
     return jsonify({"day": day, "confirm_add": confirm_add, "suspect_add": suspect_add})
 
+
 @app.route("/r1")
 def get_r1_data():
     data = utils.get_r1_data()
     city = []
     confirm = []
-    for k,v in data:
+    for k, v in data:
         city.append(k)
         confirm.append(int(v))
     return jsonify({"city": city, "confirm": confirm})
@@ -61,7 +66,7 @@ def get_r1_data():
 
 @app.route("/r2")
 def get_r2_data():
-    data = utils.get_r2_data() #格式 (('民警抗疫一线奋战16天牺牲1037364',), ('四川再派两批医疗队1537382',)
+    data = utils.get_r2_data()  # 格式 (('民警抗疫一线奋战16天牺牲1037364',), ('四川再派两批医疗队1537382',)
     d = []
     for i in data:
         k = i[0].rstrip(string.digits)  # 移除热搜数字
@@ -72,37 +77,11 @@ def get_r2_data():
                 d.append({"name": j, "value": v})
     return jsonify({"kws": d})
 
+
 @app.route("/time")
 def get_time():
     return utils.get_time()
 
-@app.route('/ajax',methods=["get","post"])
-def hello_world4():
-    name = request.values.get("name")
-    score =  request.values.get("score")
-    print(f"name:{name},score:{score}")
-    return '10000'
-
-@app.route('/tem')
-def hello_world3():
-    return render_template("index.html")
-
-@app.route('/login')
-def hello_world2():
-    name =  request.values.get("name")
-    pwd =  request.values.get("pwd")
-    return f'name={name},pwd={pwd}'
-
-@app.route("/abc")
-def hello_world1():
-    id = request.values.get("id")
-    return f"""
-    <form action="/login">
-        账号：<input name="name" value="{id}"><br>
-        密码：<input name="pwd">
-        <input type="submit">
-    </form>
-    """
 
 if __name__ == '__main__':
     app.run()
