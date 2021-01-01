c589fd610dbfda37a885a92a85d1a73fe5b0d14e
Dark0Spirit <2424592449@qq.com>
2021-01-01 22:52:59 +0800

    format code

--- static/js/ec_right1.js
+++ static/js/ec_right1.js
@@ -1,20 +1,20 @@
-var ec_right1 = echarts.init(document.getElementById('r1'),"dark");
+var ec_right1 = echarts.init(document.getElementById('r1'), "dark");
 var ec_right1_option = {
-	//标题样式
-	title : {
-	    text : "非湖北地区城市确诊TOP5",
-	    textStyle : {
-	        color : 'white',
-	    },
-	    left : 'left'
-	},
-	  color: ['#3398DB'],
-	    tooltip: {
-	        trigger: 'axis',
-	        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-	            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-	        }
-	    },
+    //标题样式
+    title: {
+        text: "非湖北地区城市确诊TOP5",
+        textStyle: {
+            color: 'white',
+        },
+        left: 'left'
+    },
+    color: ['#3398DB'],
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
     xAxis: {
         type: 'category',
         data: []
@@ -25,7 +25,7 @@ var ec_right1_option = {
     series: [{
         data: [],
         type: 'bar',
-		barMaxWidth:"50%"
+        barMaxWidth: "50%"
     }]
 };
 ec_right1.setOption(ec_right1_option)
