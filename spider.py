20cd4bfda57b5d3714eb640ebf08db7101ab3e1b
Dark0Spirit <2424592449@qq.com>
2021-01-12 14:21:52 +0800

    format code

--- spider.py
+++ spider.py
@@ -49,26 +49,6 @@ def get_tencent_data():
     data_his = json.loads(res_his['data'])
 
     history = {}  # 历史数据
-    #     for i in data_all["chinaDayList"]:
-    #         ds = "2020." + i["date"]
-    #         tup = time.strptime(ds, "%Y.%m.%d")
-    #         ds = time.strftime("%Y-%m-%d", tup)  # 改变时间格式,不然插入数据库会报错，数据库是datetime类型
-    #         confirm = i["confirm"]
-    #         suspect = i["suspect"]
-    #         heal = i["heal"]
-    #         dead = i["dead"]
-    #         history[ds] = {"confirm": confirm, "suspect": suspect, "heal": heal, "dead": dead}
-    #     for i in data_all["chinaDayAddList"]:
-    #         ds = "2020." + i["date"]
-    #         tup = time.strptime(ds, "%Y.%m.%d")
-    #         ds = time.strftime("%Y-%m-%d", tup)
-    #         confirm = i["confirm"]
-    #         suspect = i["suspect"]
-    #         heal = i["heal"]
-    #         dead = i["dead"]
-    #         history[ds].update({"confirm_add": confirm, "suspect_add": suspect, "heal_add": heal, "dead_add": dead})
-
-    # 通过上面的代码肯定不行了，里面只有当日详细数据，修改也很简单，改一下循环遍历的数据源即可：++++
     for i in data_his["chinaDayList"]:
         ds = "2020." + i["date"]
         tup = time.strptime(ds, "%Y.%m.%d")
@@ -103,76 +83,6 @@ def get_tencent_data():
             dead = city_infos["total"]["dead"]
             details.append([update_time, province, city, confirm, confirm_add, heal, dead])
     return history, details
-
-
-def get_baidu_hot():
-    """
-    :return: 返回百度疫情热搜
-    """
-    option = ChromeOptions()  # 创建谷歌浏览器实例
-    option.add_argument("--headless")  # 隐藏浏览器
-    option.add_argument('--no-sandbox')
-
-    url = "https://voice.baidu.com/act/virussearch/virussearch?from=osari_map&tab=0&infomore=1"
-    browser = Chrome(options=option, executable_path="./chromedriver.exe")
-    browser.get(url)
-    # 找到展开按钮
-    # dl = browser.find_element_by_xpath('//*[@id="ptab-0"]/div/div[2]/section/div')
-    dl = browser.find_element_by_xpath('//*[@id="ptab-0"]/div/div[1]/section/div')
-    dl.click()
-    time.sleep(1)
-    # 找到热搜标签
-    # c = browser.find_elements_by_xpath('//*[@id="ptab-0"]/div/div[2]/section/a/div/span[2]')
-    c = browser.find_elements_by_xpath('//*[@id="ptab-0"]/div/div[1]/section/a/div/span[2]')
-    context = [i.text for i in c]  # 获取标签内容
-    print(context)
-    return context
-
-
-# def update_hotsearch():
-#     """
-#     将疫情热搜插入数据库
-#     :return:
-#     """
-#     cursor = None
-#     conn = None
-#     try:
-#         context = get_baidu_hot()
-#         print(f"{time.asctime()}开始更新热搜数据")
-#         conn, cursor = get_conn()
-#         sql = "insert into hotsearch(dt,content) values(%s,%s)"
-#         ts = time.strftime("%Y-%m-%d %X")
-#         for i in context:
-#             cursor.execute(sql, (ts, i))  # 插入数据
-#         conn.commit()  # 提交事务保存数据
-#         print(f"{time.asctime()}数据更新完毕")
-#     except:
-#         traceback.print_exc()
-#     finally:
-#         close_conn(conn, cursor)
-
-
-# def update_hotsearch():
-#     """
-#     将疫情热搜插入数据库
-#     :return:
-#     """
-#     cursor = None
-#     conn = None
-#     try:
-#         context = get_baidu_hot()
-#         print(f"{time.asctime()}开始更新热搜数据")
-#         conn, cursor = get_conn()
-#         sql = "insert into hotsearch(dt,content) values(%s,%s)"
-#         ts = time.strftime("%Y-%m-%d %X")
-#         for i in context:
-#             cursor.execute(sql, (ts, i))  # 插入数据
-#         conn.commit()  # 提交事务保存数据
-#         print(f"{time.asctime()}数据更新完毕")
-#     except:
-#         traceback.print_exc()
-#     finally:
-#         close_conn(conn, cursor)
 
 
 def update_details():
